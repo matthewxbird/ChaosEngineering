c61ffa45253c177e924a4e04470da3129eeffb1f
Matthew Bird <mbird1989@hotmail.com>
2021-05-08 18:06:47 +0100

    Add project files.

---
+++ ChaosHost/Controllers/ChaosHostController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ChaosHost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ChaosHostController : ControllerBase
+    {
+        private readonly ILogger<ChaosHostController> _logger;
+
+        public ChaosHostController(ILogger<ChaosHostController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+            var rng = new Random();
+            return rng.Next();
+        }
+    }
+}
